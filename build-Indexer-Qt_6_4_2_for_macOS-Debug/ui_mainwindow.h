0285c67d96269f18bffd6292c61f0abc50a6b6d6
Romain <r.huc@orange.fr>
2023-04-17 09:57:49 +0200

    fix ui

--- build-Indexer-Qt_6_4_2_for_macOS-Debug/ui_mainwindow.h
+++ build-Indexer-Qt_6_4_2_for_macOS-Debug/ui_mainwindow.h
@@ -26,7 +26,6 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QPushButton *pushButton;
-    QLineEdit *lineEdit;
     QCheckBox *checkBox;
     QCheckBox *checkBox_2;
     QCheckBox *checkBox_3;
@@ -34,7 +33,7 @@ public:
     QCheckBox *checkBox_5;
     QCheckBox *checkBox_6;
     QCheckBox *checkBox_7;
-    QWidget *widget;
+    QLineEdit *lineEdit;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -42,43 +41,40 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(596, 234);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(320, 130, 100, 32));
-        lineEdit = new QLineEdit(centralwidget);
-        lineEdit->setObjectName("lineEdit");
-        lineEdit->setGeometry(QRect(190, 140, 113, 21));
+        pushButton->setGeometry(QRect(300, 90, 100, 32));
         checkBox = new QCheckBox(centralwidget);
         checkBox->setObjectName("checkBox");
-        checkBox->setGeometry(QRect(340, 100, 121, 20));
+        checkBox->setGeometry(QRect(310, 60, 121, 20));
         checkBox_2 = new QCheckBox(centralwidget);
         checkBox_2->setObjectName("checkBox_2");
-        checkBox_2->setGeometry(QRect(140, 70, 91, 20));
+        checkBox_2->setGeometry(QRect(100, 30, 91, 20));
         checkBox_3 = new QCheckBox(centralwidget);
         checkBox_3->setObjectName("checkBox_3");
-        checkBox_3->setGeometry(QRect(240, 70, 91, 20));
+        checkBox_3->setGeometry(QRect(200, 30, 91, 20));
         checkBox_4 = new QCheckBox(centralwidget);
         checkBox_4->setObjectName("checkBox_4");
-        checkBox_4->setGeometry(QRect(410, 70, 85, 20));
+        checkBox_4->setGeometry(QRect(380, 30, 85, 20));
         checkBox_5 = new QCheckBox(centralwidget);
         checkBox_5->setObjectName("checkBox_5");
-        checkBox_5->setGeometry(QRect(350, 70, 85, 20));
+        checkBox_5->setGeometry(QRect(310, 30, 85, 20));
         checkBox_6 = new QCheckBox(centralwidget);
         checkBox_6->setObjectName("checkBox_6");
-        checkBox_6->setGeometry(QRect(140, 100, 51, 20));
+        checkBox_6->setGeometry(QRect(100, 60, 51, 20));
         checkBox_7 = new QCheckBox(centralwidget);
         checkBox_7->setObjectName("checkBox_7");
-        checkBox_7->setGeometry(QRect(240, 100, 61, 20));
-        widget = new QWidget(centralwidget);
-        widget->setObjectName("widget");
-        widget->setGeometry(QRect(80, 50, 481, 151));
+        checkBox_7->setGeometry(QRect(200, 60, 61, 20));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(160, 100, 113, 21));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 36));
+        menubar->setGeometry(QRect(0, 0, 596, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -100,6 +96,7 @@ public:
         checkBox_5->setText(QCoreApplication::translate("MainWindow", "SIZE", nullptr));
         checkBox_6->setText(QCoreApplication::translate("MainWindow", "EXT", nullptr));
         checkBox_7->setText(QCoreApplication::translate("MainWindow", "TYPE", nullptr));
+        lineEdit->setText(QString());
     } // retranslateUi
 
 };
